03b5852738628f2fe76567833f7a830d1eadf7ac
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-02-13 17:36:14 +0500

    finish model

--- src/model/Weather.js
+++ src/model/Weather.js
@@ -1,4 +1,20 @@
 import { Sequelize, DataTypes } from "sequelize";
 import { sequelize } from "../config/_db.js";
 
+const Forecast = sequelize.define("Weather", {
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  temperature: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  condition: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
+
+export { Forecast };
